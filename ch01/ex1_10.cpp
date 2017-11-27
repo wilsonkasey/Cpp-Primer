86285aa2545fece9c607a26cd59f3559a67c805c
Kasey Wilson <kasey.w512@gmail.com>
2017-11-27 13:47:24 -0500

    Update ex1_10.cpp
    why don't we use standard library here? As well as a better description. It makes a bit of a difference.

--- ch01/ex1_10.cpp
+++ ch01/ex1_10.cpp
@@ -1,11 +1,12 @@
-// prints the numbers from ten down to zero.(use while)
+//Prints numbers from 1 to 10 using a while loop
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
     int i = 10;
     while (i >= 0)
-        std::cout << i-- << " ";
+        cout << i-- << " ";
     return 0;
 }
